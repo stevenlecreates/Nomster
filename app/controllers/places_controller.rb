73460686933e8316ef9814f54b401e439905e57b
Steven Le <stevenlecreates@gmail.com>
2015-12-09 15:46:31 +0000

    Added function so only user who created place can update place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
 
-	before_action :authenticate_user!, :only => [:new, :create, :edit]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :update]
 
 	def index
  	 @places=Place.paginate(:page => params[:page], :per_page => 5)
@@ -32,6 +32,9 @@ class PlacesController < ApplicationController
 
 	def update
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
 		@place.update_attributes(place_params)
 		redirect_to root_path
 	end
