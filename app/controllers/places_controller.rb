dfe9b1e55147a1312daa893d4705e86a1a6983f6
Steven Le <stevenlecreates@gmail.com>
2015-12-09 15:29:35 +0000

    Added function so only user who created place can edit place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
 
-	before_action :authenticate_user!, :only => [:new, :create]
+	before_action :authenticate_user!, :only => [:new, :create, :edit]
 
 	def index
  	 @places=Place.paginate(:page => params[:page], :per_page => 5)
@@ -25,6 +25,9 @@ class PlacesController < ApplicationController
 
 	def edit
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+    	return render :text => 'Not Allowed', :status => :forbidden
+  	end
 	end
 
 	def update
