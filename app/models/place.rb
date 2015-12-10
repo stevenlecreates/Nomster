b2589c86610a8e9c4eb4cb88413494f2342d950b
Steven Le <stevenlecreates@gmail.com>
2015-12-10 12:07:14 +0000

    Added function to show latest comment on homepage

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,11 +1,15 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
-	
+
 	geocoded_by :address
 	after_validation :geocode
 
 	validates :name, :presence => true, length: { minimum: 3 }
 	validates :description, :presence => true
 	validates :address, :presence => true
+
+	def last_comment
+      self.comments.order("id ASC").last
+ 	end
 end
