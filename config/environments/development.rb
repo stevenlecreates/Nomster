91711b442be8a10e611fd6c86ae095a076cae392
Steven Le <stevenlecreates@gmail.com>
2015-12-13 16:56:49 +0000

    Adjusted the automated email mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 Nomster::Application.configure do
-  
+
    config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
@@ -11,8 +11,8 @@ Nomster::Application.configure do
     user_name: ENV['GMAIL_ADDRESS'],
     password: ENV['GMAIL_PASSWORD']
   }
-
-  config.action_mailer.default_url_options = { :host => 'localhost: 3030'}
+  config.action_mailer.default_url_options = { :host => 'localhost:3030' }
+  
 
   # Settings specified here will take precedence over those in config/application.rb.
 
